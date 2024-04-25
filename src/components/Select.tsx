d5cf313fa08bd559d5006cc7e0d24258911a793c
Yury Odintsov <yury.odinson@gmail.com>
2024-04-25 23:25:42 +0300

    feat: select and render filtered users

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from "react";
-import {FilterContext} from "../tools/utils";
+import {FilterContext, updateData, updateOptions} from "../tools/utils";
 import {SelectProps} from "../tools/types";
 
 export const Select: React.FC<SelectProps> = ({names}) => {
@@ -11,29 +11,29 @@ export const Select: React.FC<SelectProps> = ({names}) => {
     const filter = useContext(FilterContext);
 
     useEffect(() => {
-        if (filter.filter.length <= 0) {
+        if (filterString === "") {
             setOptionValues(names);
         } else {
-            setOptionValues(filter.filter);
+            updateOptions(names, filter.filter);
         }
     }, [filter.filter, names, filterString]);
 
-    const handlerChangeStyle = () => setIsOpened(!isOpened);
-
-    const option = (value: string) => (
-        <label className="select-options__item" key={value}>
-            <input type="checkbox"/>
-            <p>{value}</p>
-        </label>
-    );
-
-    const searchInUsers = (value: any) => {
+    const searchInUsers = (value: string) => {
         setFilterString(value);
         const filteredNames = names.filter(name => {
             return name.toLowerCase().includes(value.toLowerCase())
         });
-        filter.setFilter(filteredNames);
-        setOptionValues(filter.filter);
+        setOptionValues(filteredNames);
+    }
+
+    const setFilterArray = (value: string) => {
+        const filteredArray = filter.filter;
+        if (filteredArray.includes(value)) {
+            filter.setFilter(filteredArray.filter(item => item !== value));
+            // updateData()
+        } else {
+            filter.setFilter([...filteredArray, value]);
+        }
     };
 
     // background - for close options pop-up
@@ -41,25 +41,32 @@ export const Select: React.FC<SelectProps> = ({names}) => {
         <div className="select-background" onClick={() => setIsOpened(false)}/>
     );
 
+    const option = (value: string) => (
+        <label className="select-options__item" key={value}>
+            <input type="checkbox" onChange={() => {
+                setFilterArray(value)
+            }}/>
+            <p>{value}</p>
+        </label>
+    );
+
     return (
-        <>
-            <div className={isOpened ? "select-wrapper-active" : "select-wrapper"}>
+        <div className={isOpened ? "select-wrapper-active" : "select-wrapper"}>
+            <div className="select" onClick={() => setIsOpened(!isOpened)}>
+                <input className="select-input" type="text" onChange={(e) => {
+                    setFilterString(e.target.value)
 
-
-                <div className="select" onClick={handlerChangeStyle}>
-                    <input className="select-input" type="text" onChange={(e) => {
-                        searchInUsers(e.target.value)
-                    }}/>
-                    <div className={isOpened ? "select-options-active" : "select-options"}>
-                        {
-                            optionValues?.map((e) => (
-                                option(e)
-                            ))
-                        }
-                        {isOpened ? background() : null}
-                    </div>
+                    searchInUsers(e.target.value)
+                }}/>
+                <div className={isOpened ? "select-options-active" : "select-options"}>
+                    {
+                        optionValues?.map((e) => (
+                            option(e)
+                        ))
+                    }
+                    {isOpened ? background() : null}
                 </div>
             </div>
-        </>
+        </div>
     );
 };
--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -15,3 +15,7 @@ export const getData = async (): Promise<any> => {
 export const updateData = (dataArr: CardProps[], filteredArr: string[]) => {
     return dataArr.filter(user => filteredArr.includes(user.name));
 };
+
+export const updateOptions = (fullData: string[], filteredData: string[]) => {
+    return fullData.filter(user => filteredData.includes(user));
+};

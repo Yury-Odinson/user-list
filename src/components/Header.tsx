878f66a7abc80258d1949511370de4830fefebd8
Yury Odintsov <yury.odinson@gmail.com>
2024-04-26 21:19:29 +0300

    build

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return (
-        <header>
-            <div className="header-wrapper">
-                <p>User list</p>
-            </div>
-        </header>
-    );
-};
